f04c5aee51edfa206b5c4437770986059f78986c
David Berlin <47757213+davbree@users.noreply.github.com>
2019-08-11 14:10:10 +0300

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,7 +7,7 @@ set -v
 if [[ -z "${STACKBIT_API_KEY}" ]]; then
     echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
 else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://b8883b02.ngrok.io/pull/5d4fe633315b34d57ba27004 
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://2eb056b9.ngrok.io/pull/5d4fe633315b34d57ba27004 
 fi
 ./ssg-build.sh
 
